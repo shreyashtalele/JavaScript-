b3286c4b03bc157f93bf5c6f1511d2933c85aa10
shreyashtalele <taleleshreyash44@gmail.com>
2026-06-22 23:09:25 +0530

    22/06 array problems binary search

--- Arrays/array_medium._problems.js
+++ Arrays/array_medium._problems.js
@@ -1,3 +1,5 @@
+
+// //Linear Search Problems
 // // Find an element in an array.
 // array = [10, 20, 30, 40, 50, 30, 70, 80]
 // function linearSearch(array, target) {
@@ -60,44 +62,170 @@
 // }
 // console.log(printLastOccurence(array, 30));
 
-// Binary Search
-function binarySearch(array, target) {
-    let left = 0
-    let right = array.length - 1
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2)
+// Binary Search Problems 
+// Binary Search ( iterative)
+// function binarySearch(array, target) {
+//     let left = 0
+//     let right = array.length - 1
+//     while (left <= right) {
+//         let mid = Math.floor((left + right) / 2)
 
-        if (array[mid] === target) {
-            return mid
+//         if (array[mid] === target) {
+//             return mid
+//         }
+//         else if (target < array[mid]) {
+//             right = mid - 1
+//         }
+//         else {
+//             left = mid + 1
+//         }
+//     }
+//     return -1
+// }
+// let sorted_array = [10, 20, 30, 40, 50, 60, 70]
+// console.log(binarySearch(sorted_array, 10));
+
+
+
+// // Binary Search ( recursive)
+
+// function binarySearchRecursive(array, target, left, right) {
+//     let mid = Math.floor((left + right) / 2)
+//     if (array[mid] === target) {
+//         return mid
+//     }
+//     else if (target < array[mid]) {
+//         return binarySearchRecursive(array, target, left, mid - 1)
+//     } else {
+//         return binarySearchRecursive(array, target, mid + 1, right)
+//     }
+// }
+
+// console.log(binarySearchRecursive(sorted_array, 50, 0, sorted_array.length - 1));
+
+// Find the first occurrence of an element in a sorted array.   
+// function findFirstOccurrenceofElement(array, target) {
+//     // if (array.length < 1 && array[0] === target) return "element found "
+
+//     if (array.length === 0) {
+//         return -1;
+//     }
+//     let left = 0
+//     let right = array.length - 1
+//     let result = -1
+
+//     while (left <= right) {
+//         let mid = Math.floor((left + right) / 2)
+
+//         if (array[mid] === target) {
+//             result = mid
+//             right = mid - 1
+//         }
+//         else if (target > array[mid]) {
+//             left = mid + 1
+//         }
+//         else {
+//             right = mid - 1
+//         }
+//     }
+//     return result
+// }
+
+// console.log(findFirstOccurrenceofElement([2, 4, 4, 4, 7, 9], 4));
+
+
+// Find the first occurrence of an element in a sorted array. 
+
+// function findLastOccurrenceofElement(array, target) {
+//     let left = 0
+//     let right = array.length - 1
+//     let result = -1
+//     while (left <= right) {
+//         let mid = Math.floor((left + right) / 2)
+
+//         if (array[mid] === target) {
+//             result = mid
+//             left = mid + 1
+//         }
+//         else if (target > array[mid]) {
+//             left = mid + 1
+//         }
+//         else {
+//             right = mid - 1
+//         }
+//     }
+
+//     return result
+
+// }
+
+// console.log(findLastOccurrenceofElement([2, 4, 4, 4, 7, 9], 4));
+
+
+
+//Count occurrences using Binary Search
+
+function countOccurrencesofElement(array, target) {
+
+    function firstoccur(array, target) {
+        let left = 0
+        let right = array.length - 1
+        let result = -1
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2)
+
+            if (array[mid] === target) {
+                result = mid
+                right = mid - 1
+            }
+            else if (target > array[mid]) {
+                left = mid + 1
+            }
+            else {
+                right = mid - 1
+            }
+
         }
-        else if (target < array[mid]) {
-            right = mid - 1
-        }
-        else {
-            left = mid + 1
-        }
+        return result
     }
-    return -1
+
+
+    function lastOccur(array, target) {
+        let left = 0
+        let right = array.length - 1
+        let result = -1
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2)
+
+            if (array[mid] === target) {
+                result = mid
+                left = mid + 1
+            }
+            else if (target > array[mid]) {
+                left = mid + 1
+            }
+            else {
+                right = mid - 1
+            }
+        }
+        return result
+    }
+
+    // console.log(firstoccur(array, target));
+    // console.log(lastOccur(array, target));
+
+
+    let first = firstoccur(array, target);
+    let last = lastOccur(array, target);
+
+    if (first === -1) {
+        return 0
+    }
+
+    return last - first + 1
+
 }
-let sorted_array = [10, 20, 30, 40, 50, 60, 70]
-console.log(binarySearch(sorted_array, 10));
-
-
-
-// Binary Search ( recursive)
-
-function binarySearchRecursive(array, target, left, right) {
-    let mid = Math.floor((left + right) / 2)
-    if (array[mid] === target) {
-        return mid
-    }
-    else if (target < array[mid]) {
-        return binarySearchRecursive(array, target, left, mid - 1)
-    } else {
-        return binarySearchRecursive(array, target, mid + 1, right)
-    }
-}
-
-console.log(binarySearchRecursive(sorted_array, 50, 0, sorted_array.length - 1));
-
+console.log(
+    countOccurrencesofElement([2, 4, 4, 4, 7, 9], 4));
 
